71e1e8c226701bf8bdb72873e271eeb4ab5ba752
n8sthegr8est <naa73@drexel.edu>
2018-02-08 15:26:39 -0500

    added test ui scene and made menu that pops up when pushing ecsape. Ui non-functional.

---
+++ 2020/Assets/Scripts/UI/Menu_on_escape.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_on_escape : MonoBehaviour {
+    [SerializeField]
+    private GameObject Menu_Pane;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!Menu_Pane.activeInHierarchy)
+            {
+                Menu_Pane.SetActive(true);
+            }
+            else
+            {
+                Menu_Pane.SetActive(false);
+            }
+        }
+	}
+}
