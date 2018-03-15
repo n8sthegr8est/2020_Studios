fd240f7fecac9c53929ca44de753b6b83d5dc6af
ztcric <tz85@drexel.edu>
2018-03-15 15:19:00 -0400

    Ui

---
+++ 2020/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour {
+
+	public Button resume;
+	//public Button restart;
+	public Button quit;
+	public void Pause()
+	{
+		
+
+
+
+		resume.gameObject.SetActive (true);
+		//restart.gameObject.SetActive (true);
+		quit.gameObject.SetActive (true);
+		gameObject.SetActive (false);
+		Time.timeScale = 0;
+	}
+}
---
+++ 2020/Assets/Scripts/ResumeGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResumeGame : MonoBehaviour {
+
+	public Button resume;
+	//public Button restart;
+	public Button quit;
+	public Button Pause;
+
+
+	public void Resume()
+	{
+
+		resume.gameObject.SetActive (false);
+		//restart.gameObject.SetActive (false);
+		quit.gameObject.SetActive (false);
+		Pause.gameObject.SetActive (true);
+
+		Time.timeScale = 1;
+
+
+
+	}
+
+}
